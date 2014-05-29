74f85673fc9d0fb0dfe5fdca830492e7c40de7ad
Artur Akchurin <akartkam@gmail.com>
2014-05-30 00:21:39 +0400

    Signed-off-by: Artur Akchurin <akartkam@gmail.com>

--- HibernateFirst/src/hello/User.java
+++ HibernateFirst/src/hello/User.java
@@ -6,14 +6,20 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "USER", schema = "MKYONGDB")
 public class User implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue
 	@Column(name = "USER_ID")
@@ -26,7 +32,7 @@ public class User implements Serializable {
     @Column(name = "PSWD")
     private String password;
     
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Bid> bids = new ArrayList<Bid>();
 
 
@@ -35,6 +41,39 @@ public class User implements Serializable {
      * No-arg constructor for JavaBean tools.
      */
     public User() {}
+    
+    public User(String username, String password) {
+    	this.username = username;
+    	this.password = password;
+    }
+    
+    public List<Bid> getBids() { return bids; }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
 	
 
 }
